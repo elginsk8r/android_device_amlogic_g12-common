bb1a154c213d4f9d54d774e0baac7f1df4256f15
Rohan Hasabe <rohanhasabe8@gmail.com>
2021-08-06 09:16:57 +0200

    g12: support non-dynamic partitions
    Change-Id: Ic1dd422bc52379323cf4a54e1cc7e285de7b1785

--- g12.mk
+++ g12.mk
@@ -106,13 +106,17 @@ PRODUCT_PACKAGES += \
 
 # Init-Files
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/init-files/fstab.amlogic:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.amlogic \
     $(COMMON_PATH)/init-files/init.amlogic.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.rc \
     $(COMMON_PATH)/init-files/init.amlogic.board.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.board.rc \
     $(COMMON_PATH)/init-files/init.amlogic.media.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc \
     $(COMMON_PATH)/init-files/init.amlogic.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.usb.rc \
     $(COMMON_PATH)/init-files/init.recovery.amlogic.rc:recovery/root/init.recovery.amlogic.rc \
     $(COMMON_PATH)/init-files/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+
+ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/init-files/fstab.amlogic:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.amlogic
+endif
 
 ## Kernel Modules
 PRODUCT_PACKAGES += \
@@ -156,10 +160,12 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 ## Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS ?= true
+ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
 BOARD_BUILD_SUPER_IMAGE_BY_DEFAULT  := true
 BUILDING_SUPER_EMPTY_IMAGE := true
 PRODUCT_BUILD_SUPER_PARTITION := true
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+endif
 
 ## Permissions
 PRODUCT_COPY_FILES +=  \
