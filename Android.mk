eab91cc84bbe0f1ff279a0e4ff71e4abdc71cb45
Nick Reuter <nreuter85@gmail.com>
2021-12-24 16:51:48 -0600

    g12-common: support gondul mali blobs for g12b devices
    Change-Id: Ib578353eb67909ed52b4fb7e23f3c7992365732a

--- Android.mk
+++ Android.mk
@@ -9,4 +9,17 @@ ifneq ($(filter g12a g12b sm1,$(TARGET_AMLOGIC_SOC)),)
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+ifneq ($(filter g12b,$(TARGET_AMLOGIC_SOC)),)
+GPU_TYPE := gondul_ion
+endif
+GPU_TYPE ?= dvalin_ion
+
+GPU_LIBRARY_SYMLINK := $(TARGET_OUT_VENDOR)/vendor/lib/egl
+$(GPU_LIBRARY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating wifi firmware symlinks: $@"
+	mkdir -p $@
+	$(hide) ln -sf /vendor/lib/egl/libGLES_$(GPU_TYPE).so $@/libGLES_mali.so
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GPU_LIBRARY_SYMLINK)
+
 endif
